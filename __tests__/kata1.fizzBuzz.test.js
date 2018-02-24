09d10ae53891b852f48610492e820164921edc85
Anthony <anthonyw22@gmail.com>
2018-02-24 16:28:50 +0000

    fixed fizzBuzz test

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -2,18 +2,18 @@ import { fizzBuzz } from '../src'
 
 describe('fizzBuzz', () => {
   it('returns Fizz when passed a multiple of 3', () => {
-
+    expect(fizzBuzz(99)).toBe('Fizz');
   })
 
   it('returns Buzz when passed a multiple of 5', () => {
-
+    expect(fizzBuzz(10)).toBe('Buzz');
   })
 
   it('returns FizzBuzz when passed a multiple 3 and 5', () => {
-
+    expect(fizzBuzz(30)).toBe('FizzBuzz');
   })
 
   it('returns the number when it isn\'t a multiple of 3 or 5', () => {
-
+    expect(fizzBuzz(16)).toBe(16);
   })
 })
