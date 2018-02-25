f0b44b4b9f6796d8b7c0016fd3f504986b65e2fd
Anthony <anthonyw22@gmail.com>
2018-02-25 18:39:16 +0000

    fixed kata 2 tests

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -2,4 +2,8 @@ import { booleanToWord } from '../src'
 
 describe('booleanToWord', () => {
   // how do we create specs again???
+  it('returns yes when passed true and no when passed false', () => {
+    expect(booleanToWord(true)).toBe('yes');
+    expect(booleanToWord(false)).toBe('no');
+  });
 })
--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,9 @@
 const booleanToWord = (boolean) => {
-
+if (boolean === true){
+  return 'yes';
+  } else if (boolean === false){
+  return 'no';
+  }
 }
 
 export default booleanToWord
