dc4ecf6221aa32493e2a93840034c4106c63eafd
Anthony <anthonyw22@gmail.com>
2018-02-25 20:46:42 +0000

    nearly fixed kata4 test

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 import { humanCatDogYears } from '../src'
 
 // Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+  it('returns the human year, dog and cat years', () => {
+    const expected = ["45", "239", "196"];
+    expect(humanCatDogYears('45')).toEqual(expected);
+  })
+})
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,20 @@
 const humanCatDogYears = (number) => {
-
+  let dogAge = 0;
+  let catAge = 0;
+  if (number === 1){
+    dogAge = 15;
+    catAge = 15;
+  } 
+  if (number === 2){
+    dogAge = 15 + 9;
+    catAge = 15 + 9;
+  } 
+  if (number >= 3){
+    const remain = number - 2;
+    dogAge = (15 + 9) + (remain * 5);
+    catAge = (15 + 9) + (remain * 4);
+  }
+  return [number, dogAge, catAge];
 }
 
 export default humanCatDogYears
